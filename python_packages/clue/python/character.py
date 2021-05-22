1990487ded6bd88f76fd93dc928622d6a5e92adb
Zachary Bialik <zachbialik1207@gmail.com>
2021-05-22 10:31:09 -0400

    wipe

--- python_packages/clue/python/character.py
+++
@@ -1,8 +0,0 @@
-class Character:
-
-  def __init__(self, character_name, home):
-        self.character_name = character_name
-        self.home = home
-        self.current_location = home
-        self.wasMovedToRoom = False
-        self.active = False
--- python_packages/dynamodb_json/python/dynamodb_json/__init__.py
+++
@@ -1,3 +0,0 @@
-__author__ = 'Alon Reznik'
-__license__ = 'Mozilla'
-__version__ = '1.3'
