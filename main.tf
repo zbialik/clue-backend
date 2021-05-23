8d84bc72fa70e58481fd5bed971325170e09d1c6
Zachary Bialik <zachbialik1207@gmail.com>
2021-05-23 11:14:23 -0400

    pushing main.tf

---
+++ main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+  required_version = ">= 0.14.9"
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-1" 
+}
+
+resource "aws_lambda_function" "create_game_template" {
+  filename      = "src/create_game_template.zip"
+  function_name = "delete-game"
+  role          = "arn:aws:iam::228573559958:role/service-role/ClueLamdaBaseRole"
+  handler       = "lambda_function.lambda_handler"
+  layers = [
+    "arn:aws:lambda:us-east-1:228573559958:layer:dynamodb_json:1",
+    "arn:aws:lambda:us-east-1:228573559958:layer:clue:1"
+  ]
+
+  # The filebase64sha256() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
+  # source_code_hash = "${base64sha256(file("lambda_function_template.zip"))}"
+  source_code_hash = filebase64sha256("src/create_game_template.zip")
+
+  runtime = "python3.8"
+
+  environment {
+    variables = {
+      table = "CLUE_GAMES"
+    }
+  }
+}
--- src/lambda_function.py
+++ src/lambda_function.py
@@ -5,6 +5,7 @@ import json
 from decimal import Decimal
 from dynamodb_json import json_util as dynamo_json
 from game import Game
+from uuid import uuid4
 
 def lambda_handler(event, context):
     
@@ -14,20 +15,16 @@ def lambda_handler(event, context):
     
     # get next gameId code
     try:
-        counter_item_dynamo = table.get_item(Key={'game_id': 1})
         
-        counter_item = dynamo_json.loads(counter_item_dynamo['Item'])
-        table_item_count = counter_item['counter']
-        print('Number of items in CLUE_GAMES table: ' + str(table_item_count))
+        # Generate Game UUID
+        new_game_id = str(uuid4())
+        print('Next new_game_id is: ' + new_game_id)
         
-        newGameId = table_item_count + 1
-        print('Next gameId is: ' + str(newGameId))
-        
-        newGame = Game(newGameId)
+        new_game = Game(new_game_id)
         
         # Add New Game Item to Table
         create_game_response_dynamo = table.put_item(
-            Item=newGame.__dict__
+            Item=new_game.__dict__
         )
         
         create_game_response = dynamo_json.loads(create_game_response_dynamo)
@@ -35,27 +32,9 @@ def lambda_handler(event, context):
         # Check Status Code of Put Item is 200
         if create_game_response['ResponseMetadata']['HTTPStatusCode'] == 200:
             print('Put Item was succesful in dynamodb. Incrementing game counter item now.')
-            
-            response_dynamo = table.update_item(
-                Key={'game_id': 1},
-                UpdateExpression="set #c = #c + :inc",
-                ExpressionAttributeValues={
-                    ':inc': 1
-                },
-                ExpressionAttributeNames={
-                    "#c": "counter"
-                },
-                ReturnValues="UPDATED_NEW"
-            )
-            
-            response = dynamo_json.loads(response_dynamo)
-            
-            if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-                print('Succesfully updated game counter.')
-            else:
-                print('Something went wrong while updating game counter item in dynamodb.')
         else: 
             print('Something messed up while putting new Game item in dynamodb.')
+            raise
         
         response = {
             "statusCode": 200,
@@ -63,7 +42,7 @@ def lambda_handler(event, context):
                 "Content-Type": "application/json"
             },
             "isBase64Encoded": False,
-            "body": newGame.__dict__
+            "body": new_game.__dict__
         }
         
         return response
