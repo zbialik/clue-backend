06cb21af65c636ad2070a8e5badefcdd8c66de85
Zachary Bialik <zachbialik1207@gmail.com>
2021-05-23 18:42:46 -0400

    delete delete-game

--- main.tf
+++ main.tf
@@ -50,27 +50,3 @@ resource "aws_lambda_function" "create_game" {
     }
   }
 }
-
-resource "aws_lambda_function" "delete_game" {
-  filename      = "src/lambda/functions/create-game/lambda_function.zip"
-  function_name = "delete-game"
-  role          = "arn:aws:iam::228573559958:role/service-role/ClueLamdaBaseRole"
-  handler       = "lambda_function.lambda_handler"
-  layers = [
-    "arn:aws:lambda:us-east-1:228573559958:layer:dynamodb_json:1",
-    "arn:aws:lambda:us-east-1:228573559958:layer:clue:1"
-  ]
-
-  # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_template.zip"))}"
-  source_code_hash = filebase64sha256("src/lambda/functions/create-game/lambda_function.zip")
-
-  runtime = "python3.8"
-
-  environment {
-    variables = {
-      table = "CLUE_GAMES"
-    }
-  }
-}
