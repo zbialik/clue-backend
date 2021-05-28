142f6a3d2f51b6c9796aabcfbab83895059249c0
Zachary Bialik <zachbialik1207@gmail.com>
2021-05-28 12:36:21 -0400

    zipping as part of script

--- .github/workflows/zip_lambda_deployables.sh
+++ .github/workflows/zip_lambda_deployables.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # A script for packaging lambda deployables prior to deployment
 
-echo "starting zip of lambda layer"
 
+# LAYER WORKFLOW
 # get list of layers
+echo "starting packaging of lambda layers"
 LAYER_FOLDERS=$(ls -d src/lambda/layers/*)
 
 # zip layer payloads
@@ -11,5 +12,16 @@ for layer in $LAYER_FOLDERS; do
     echo "zipping layer: $layer"
     zip -r $layer/layer $layer/python/
 done
+echo "packaging of lambda layers complete"
 
-echo "zipping of lambda layers complete"
+# FUNCTION WORKFLOW
+# get list of functions
+echo "starting packaging of lambda functions"
+FUNCTION_FOLDERS=$(ls -d src/lambda/functions/*)
+
+# zip function payloads
+for func in $FUNCTION_FOLDERS; do
+    echo "zipping function: $func"
+    zip -r $func/lambda_function.zip $func/lambda_function.py
+done
+echo "packaging of lambda functions complete"
