ba2086af5094e94d5590cd21ffba95d6d5bbe3b0
Zachary Bialik <zachbialik1207@gmail.com>
2021-05-28 12:14:25 -0400

    pushing updated files

--- .github/workflows/zip_lambda_functions.sh
+++ .github/workflows/zip_lambda_functions.sh
@@ -2,3 +2,11 @@
 # A script for packaging lambda functions prior to deployment
 
 echo Starting Zip Lambda functions
+
+# get list of layers
+LAYER_FOLDERS=$(ls -d src/lambda/layers/*)
+
+
+# zip layer payloads
+zip -r src/lambda/layers/dynamodb_json/layer src/lambda/layers/dynamodb_json/python/
+
---
+++ test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# A script for packaging lambda functions prior to deployment
+
+echo "starting zip of lambda layer"
+
+# get list of layers
+LAYER_FOLDERS=$(ls -d src/lambda/layers/*)
+
+# zip layer payloads
+for layer in $LAYER_FOLDERS; do
+    echo "zipping layer: $layer"
+    zip -r $layer/layer $layer/python/
+done
+
+echo "zipping of lambda layers complete"
