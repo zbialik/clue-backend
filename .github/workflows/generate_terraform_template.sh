54ee0cb9b16c7fc2b1ad6c15a07c1ff7aa68673c
Zachary Bialik <zachbialik1207@gmail.com>
2021-05-29 09:09:57 -0400

    reorganized

--- .github/workflows/generate_terraform_template.sh
+++ .github/workflows/generate_terraform_template.sh
@@ -4,11 +4,12 @@
 # Set Constants
 WORKSPACE=$(pwd)
 TEMP_RESOURCE_FILE="temp_resource.tf"
-TEMPLATE_LAMBDA_FUNCTION="resource_lambda_function.tf"
-TEMPLATE_LAMBDA_LAYER="resource_lambda_layer.tf"
+TEMPLATES_DIR=".github/workflows/templates"
+TEMPLATE_LAMBDA_FUNCTION="${TEMPLATES_DIR}/resources/lambda_function.tf"
+TEMPLATE_LAMBDA_LAYER="${TEMPLATES_DIR}/resources/lambda_layer.tf"
 
 # Copy Main Template File
-cp .github/workflows/templates/main.tf main.tf
+cp ${TEMPLATES_DIR}/main.tf main.tf
 
 # Loop Through Appending Layers
 LAYER_FOLDERS=$(ls -d ${WORKSPACE}/src/lambda/layers/*)
@@ -26,7 +27,7 @@ for layer in $LAYER_FOLDERS; do
     echo '    '$line_to_add >> temp.txt
 
     # cp template to temp file
-    cp -rf .github/workflows/templates/$TEMPLATE_LAMBDA_LAYER $TEMP_RESOURCE_FILE
+    cp -rf $TEMPLATE_LAMBDA_LAYER $TEMP_RESOURCE_FILE
 
     # tokenize temp file
     LAYER_ZIP_PATH_FOR_SED=$(echo $LAYER_ZIP_PATH | sed 's/\//\\\//g')
@@ -52,7 +53,7 @@ for func in $FUNCTION_FOLDERS; do
     echo "FUNCTION_NAME: $FUNCTION_NAME"
 
     # cp template to temp file
-    cp -rf .github/workflows/templates/$TEMPLATE_LAMBDA_FUNCTION $TEMP_RESOURCE_FILE
+    cp -rf $TEMPLATE_LAMBDA_FUNCTION $TEMP_RESOURCE_FILE
 
     # tokenize temp file
     FUNCTION_ZIP_PATH_FOR_SED=$(echo $FUNCTION_ZIP_PATH | sed 's/\//\\\//g')
