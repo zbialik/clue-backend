e7f9608be939ca94c94ebbef9c9c28e84191c610
Zachary Bialik <zachbialik1207@gmail.com>
2021-05-30 21:48:01 -0400

    added back dynamodb_json layer

--- .github/workflows/templates/resources/lambda_function.tf
+++ .github/workflows/templates/resources/lambda_function.tf
@@ -7,10 +7,7 @@ resource "aws_lambda_function" "__FUNCTION_NAME__" {
     aws_lambda_layer_version.clue.arn,
     aws_lambda_layer_version.dynamodb_json.arn
   ]
-
-  # The filebase64sha256() function is available in Terraform 0.11.12 and later
-  # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
-  # source_code_hash = "${base64sha256(file("lambda_function_template.zip"))}"
+  
   source_code_hash = filebase64sha256("__FUNCTION_ZIP_PATH__")
 
   runtime = "python3.8"
