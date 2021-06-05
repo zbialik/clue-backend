735ca4998c452d1e6a27fd3e0120ea2010ae3b5c
Zachary Bialik <zachbialik1207@gmail.com>
2021-06-05 15:27:38 -0400

    baseline new clue-backend

--- .github/workflows/templates/resources/api_lambda_integration.tf
+++ .github/workflows/templates/resources/api_lambda_integration.tf
@@ -19,8 +19,6 @@ resource "aws_lambda_permission" "apigw_lambda__API_GATEWAY_RESOURCE_NAME_____AP
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.__FUNCTION_NAME__.function_name
   principal     = "apigateway.amazonaws.com"
-
-  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
   source_arn = "arn:aws:execute-api:us-east-1:228573559958:${aws_api_gateway_rest_api.__API_GATEWAY_REST_API_NAME__.id}/*/${aws_api_gateway_method.__API_GATEWAY_RESOURCE_NAME_____API_GATEWAY_HTTP_METHOD__.http_method}${aws_api_gateway_resource.__API_GATEWAY_RESOURCE_NAME__.path}"
 }
 
