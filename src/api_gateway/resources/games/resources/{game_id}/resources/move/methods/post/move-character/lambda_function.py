64653f834d3e0e4dca05909bd5329d84624dfc46
Zachary Bialik <zachbialik1207@gmail.com>
2021-06-06 15:01:14 -0400

    initialize method

--- src/api_gateway/resources/games/resources/{game_id}/resources/move/methods/post/move-character/lambda_function.py
+++ src/api_gateway/resources/games/resources/{game_id}/resources/move/methods/post/move-character/lambda_function.py
@@ -8,48 +8,50 @@ from game import Game
 
 def lambda_handler(event, context):
     
-    print('executing start-game lambda function')
+    print('executing move-character lambda function')
     db = boto3.resource('dynamodb', region_name='us-east-1')
     table = db.Table('CLUE_GAMES')
     
-    # get next gameId code
     try:
-        print('activating game ' + str(event['game_id']) + ' now.')
+        print('up game ' + str(event['game_id']) + ' now.')
+
+        # TODO: Validate Request
         
-        response_dynamo = table.update_item(
-            Key={'game_id': event['game_id']},
-            UpdateExpression="set #a = :a",
-            ExpressionAttributeValues={
-                ':a': event['start_game']
-            },
-            ExpressionAttributeNames={
-                "#a": 'active'
-            },
-            ReturnValues="UPDATED_NEW"
-        )
+        # TODO: Replace with Update of Character Location
+        # response_dynamo = table.update_item(
+        #     Key={'game_id': event['game_id']},
+        #     UpdateExpression="set #a = :a",
+        #     ExpressionAttributeValues={
+        #         ':a': event['start_game']
+        #     },
+        #     ExpressionAttributeNames={
+        #         "#a": 'active'
+        #     },
+        #     ReturnValues="UPDATED_NEW"
+        # )
         
-        response = dynamo_json.loads(response_dynamo)
+        # response = dynamo_json.loads(response_dynamo)
         
-        if response['ResponseMetadata']['HTTPStatusCode'] == 200:
-            print('Succesfully activated game.')
+        # if response['ResponseMetadata']['HTTPStatusCode'] == 200:
+        #     print('Succesfully activated game.')
             
-            game_dynamo = table.get_item(
-                Key={'game_id': event['game_id']}
-            )
+        #     game_dynamo = table.get_item(
+        #         Key={'game_id': event['game_id']}
+        #     )
             
-            response = {
-                "statusCode": 200,
-                "headers": {
-                    "Content-Type": "application/json"
-                },
-                "isBase64Encoded": False,
-                "body": dynamo_json.loads(game_dynamo['Item'])
-            }
+        #     response = {
+        #         "statusCode": 200,
+        #         "headers": {
+        #             "Content-Type": "application/json"
+        #         },
+        #         "isBase64Encoded": False,
+        #         "body": dynamo_json.loads(game_dynamo['Item'])
+        #     }
             
-            return response
-        else:
-            print('ERROR: something went wrong while updating "active" attribute for game item (' + str(event['game_id']) + ') in dynamodb.')
-            raise
+        #     return response
+        # else:
+        #     print('ERROR: something went wrong while updating "active" attribute for game item (' + str(event['game_id']) + ') in dynamodb.')
+        #     raise
     
     except:
         print("ERROR:", sys.exc_info()[0])
