a245cc7e104c1226f819bdd3585b5d0978d5d7ee
Zachary Bialik <zachbialik1207@gmail.com>
2021-06-13 17:40:36 -0400

    renamed lambda_function

--- .github/workflows/templates/resources/apigateway_rest_api.tf
+++
@@ -1,8 +0,0 @@
-# API Gateway
-resource "aws_api_gateway_rest_api" "__API_GATEWAY_REST_API_NAME__" {
-  name = "__API_GATEWAY_REST_API_NAME__"
-
-  endpoint_configuration {
-    types = ["REGIONAL"]
-  }
-}
