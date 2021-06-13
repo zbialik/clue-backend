e50fd92b53f680f13cfde3a68ec66a89bd9ce002
Zachary Bialik <zachbialik1207@gmail.com>
2021-06-13 17:33:22 -0400

    pushing new refactor

--- .github/workflows/build_deployables.py
+++ .github/workflows/build_deployables.py
@@ -10,13 +10,13 @@ import shutil, os
 # set local variables
 WORKSPACE = os.getcwd()
 LAMBDA_LAYERS_DIR = WORKSPACE + '/src/lambda/layers'
+LAMBDA_FUNCTIONS_DIR = WORKSPACE + '/src/lambda/functions'
 TERRAFORM_TEMPLATE_PATH = WORKSPACE + '/main.tf'
 TEMPLATE_LAMBDA_FUNCTION= utils.TEMPLATES_DIR + "/resources/lambda_function.tf"
 TEMPLATE_LAMBDA_LAYER= utils.TEMPLATES_DIR + "/resources/lambda_layer.tf"
 TEMPLATE_API_GATEWAY_RESOURCE= utils.TEMPLATES_DIR + "/resources/apigateway_resource.tf"
 TEMPLATE_API_GATEWAY_REST_API= utils.TEMPLATES_DIR + "/resources/apigateway_rest_api.tf"
 TEMPLATE_API_GATEWAY_INTEGRATION= utils.TEMPLATES_DIR + "/resources/api_lambda_integration.tf"
-TEMPLATE_API_GATEWAY_INTEGRATION_PATH_ID= utils.TEMPLATES_DIR + "/resources/api_lambda_integration_path_id.tf"
 
 # Set Custom Variables
 API_GATEWAY_REST_API_NAME = 'clue-backend'
@@ -30,12 +30,11 @@ def api_gateway_workflow():
             path_to_resources = path_to_resources.rstrip('/')
             parent_resource = path_to_resources.split('/')[-2]
             
-            if ("{" in parent_resource) or ("}" in parent_resource):
-                parent_resource = parent_resource.replace('{','').replace('}','')
-            
             if parent_resource == ROOT_RESOURCE_NAME:
                 parent_resource_id_variable = 'aws_api_gateway_rest_api.' + API_GATEWAY_REST_API_NAME + '.root_resource_id'
             else:
+                if ("{" in parent_resource) or ("}" in parent_resource):
+                    parent_resource = parent_resource.replace('{','').replace('}','')
                 parent_resource_id_variable = 'aws_api_gateway_resource.' + parent_resource + '.id'
             
             if os.path.isdir(path_to_resources):
@@ -58,7 +57,7 @@ def api_gateway_workflow():
                             utils.append_new_line(TERRAFORM_TEMPLATE_PATH, line_to_append)
                     
                     # 2. Recurse through Subresources
-                    subresources_folder_path = path_to_resources + '/' + resource + '/' + utils.RESOURCES_DIR_NAME
+                    subresources_folder_path = path_to_resources + '/' + path_part + '/' + utils.RESOURCES_DIR_NAME
                     process_resources(subresources_folder_path)
         
         print('starting api gateway "resource" workflow')
@@ -123,8 +122,6 @@ def api_gateway_workflow():
     # append API Gateway - Integration template
     gateway_integration_workflow()
 
-
-
 def lamda_worflow():
     def layers_worfklow():
         layers_directories = utils.get_all_sub_directory_names(LAMBDA_LAYERS_DIR)
@@ -144,44 +141,13 @@ def lamda_worflow():
                     utils.append_new_line(TERRAFORM_TEMPLATE_PATH, line_to_append)
 
     def functions_worfklow():
-        def process_lambda_functions_dirs(search_dir):
-            def process_method_functions(path_to_methods):
-                if os.path.isdir(path_to_methods):
-                    method_folders = utils.get_all_sub_directory_names(path_to_methods)
-                    for method in method_folders:
-
-                        # 1. Zip Lambda Function Deployable
-                        method_folder_path = path_to_methods + '/' + method
-
-                        function_name = utils.get_all_sub_directory_names(method_folder_path)[0] # only one function per method
-                        print('processing function: ' + function_name)
-                        function_folder_path = method_folder_path + '/' + function_name
-                        
-                        function_zip_path = function_folder_path + '/lambda_function.zip'
-                        utils.zip_lambda_deployable(function_folder_path + '/' + utils.LAMBDA_FUNCTION_FILENAME, function_zip_path)
-
-                        # 2. Update Terraform Template for Lambda Function
-                        utils.append_new_line(TERRAFORM_TEMPLATE_PATH, '\n')
-                        with open(TEMPLATE_LAMBDA_FUNCTION, 'r') as reader:
-                            for line in reader:
-                                # token replacement for line of template
-                                line_to_append = line.replace('__FUNCTION_NAME__',function_name).replace('__FUNCTION_ZIP_PATH__',function_zip_path)
-                                utils.append_new_line(TERRAFORM_TEMPLATE_PATH, line_to_append)
-
-            # Process 'methods' folder for gateway resource
-            methods_folder_path = search_dir + '/' + utils.METHODS_DIR_NAME
-            process_method_functions(methods_folder_path)
-
-            # If there are subresources, update search_dir and recurse through resources
-            resources_folder_path = search_dir + '/' + utils.RESOURCES_DIR_NAME
-            if os.path.isdir(resources_folder_path):
-                resource_sub_directories = utils.get_all_sub_directory_names(resources_folder_path)
-                for resource_dir in resource_sub_directories:
-                    print('iterating through functions for resource: ' + resource_dir)
-                    process_lambda_functions_dirs(resources_folder_path + '/' + resource_dir)
-        
-        search_dir = WORKSPACE + '/src/api_gateway'
-        process_lambda_functions_dirs(search_dir)
+        function_directories = utils.get_all_sub_directory_names(LAMBDA_FUNCTIONS_DIR)
+        for function in function_directories:
+            # Zip Lambda Function Deployable
+            print('processing function: ' + function)
+            function_folder_path = LAMBDA_FUNCTIONS_DIR + '/' + function
+            function_zip_path = function_folder_path + '/lambda_function.zip'
+            utils.zip_lambda_deployable(function_folder_path + '/' + utils.LAMBDA_FUNCTION_FILENAME, function_zip_path)
 
     
     # Layers Workflow
@@ -196,14 +162,9 @@ def main():
     lamda_worflow()
     
     # API Gateway Workflow
-    api_gateway_workflow()
 
     # DynamoDB Workflow
 
-    a_file = open(TERRAFORM_TEMPLATE_PATH)
-    lines = a_file.readlines()
-    for line in lines:
-        print(line)
     
 if __name__ == "__main__":
     main()
--- main.tf
+++ main.tf
@@ -12,18 +12,91 @@ terraform {
   }
   required_version = ">= 0.14.9"
 }
-
 data "terraform_remote_state" "clue_backend" {
   backend = "s3"
   config = {
     bucket = "clue-backend"
-    key    = "terraform/state"
+    key    = "terraform/v1/state"
     region = "us-east-1"
   }
 }
-
 provider "aws" {
   profile = "default"
   region  = "us-east-1" 
 }
 
+# Lambda Layers
+resource "aws_lambda_layer_version" "dynamodb_json" {
+  filename   = "/home/runner/work/clue-backend/clue-backend/src/lambda/layers/dynamodb_json/layer.zip"
+  layer_name = "dynamodb_json"
+  compatible_runtimes = ["python3.8"]
+}
+resource "aws_lambda_layer_version" "clue" {
+  filename   = "/home/runner/work/clue-backend/clue-backend/src/lambda/layers/clue/layer.zip"
+  layer_name = "clue"
+  compatible_runtimes = ["python3.8"]
+}
+
+# Lambda Functions
+resource "aws_lambda_function" "game-controller" {
+  filename      = "/home/runner/work/clue-backend/clue-backend/src/lambda/functions/game-controller/lambda_function.zip"
+  function_name = "game-controller"
+  role          = "arn:aws:iam::228573559958:role/service-role/ClueLamdaBaseRole"
+  handler       = "lambda_function.lambda_handler"
+  layers = [
+    aws_lambda_layer_version.clue.arn,
+    aws_lambda_layer_version.dynamodb_json.arn
+  ]
+  source_code_hash = filebase64sha256("/home/runner/work/clue-backend/clue-backend/src/lambda/functions/game-controller/lambda_function.zip")
+  runtime = "python3.8"
+  environment {
+    variables = {
+      table = "CLUE_GAMES"
+    }
+  }
+}
+
+# API Gateway
+resource "aws_api_gateway_rest_api" "clue" {
+  name = "clue"
+  endpoint_configuration {
+    types = ["REGIONAL"]
+  }
+}
+
+# API Gateway Resources
+resource "aws_api_gateway_resource" "api_resource_games" {
+  path_part   = "games"
+  parent_id   = aws_api_gateway_rest_api.clue-backend.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.clue-backend.id
+}
+resource "aws_api_gateway_resource" "api_resource_game_id" {
+  path_part   = "{game_id}"
+  parent_id   = aws_api_gateway_resource.api_resource_games.id
+  rest_api_id = aws_api_gateway_rest_api.clue-backend.id
+}
+
+# API Gateway Methods
+resource "aws_api_gateway_method" "api_method_games" {
+  rest_api_id   = aws_api_gateway_rest_api.clue-backend.id
+  resource_id   = aws_api_gateway_resource.api_resource_games.id
+  http_method   = "ANY"
+  authorization = "NONE"
+}
+
+# API Gateway - Lambda Integrations and Permission
+resource "aws_api_gateway_integration" "lambda_api_integration_games" {
+  rest_api_id             = aws_api_gateway_rest_api.clue-backend.id
+  resource_id             = aws_api_gateway_resource.api_resource_games.id
+  http_method             = aws_api_gateway_method.api_method_games.http_method
+  integration_http_method = "POST"
+  type                    = "AWS_PROXY"
+  uri                     = aws_lambda_function.game-controller.invoke_arn
+}
+resource "aws_lambda_permission" "lamda_api_permissions_games" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.game-controller.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn = "arn:aws:execute-api:us-east-1:228573559958:${aws_api_gateway_rest_api.clue-backend.id}/*/${aws_api_gateway_method.api_method_games.http_method}${aws_api_gateway_resource.api_resource_games.path}"
+}
