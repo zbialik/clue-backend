800ccdf851738586c1343a5d8759a4fb5b9ee3c3
Zachary Bialik <zachbialik1207@gmail.com>
2021-06-13 17:42:33 -0400

    fix rest api renaming

--- main.tf
+++ main.tf
@@ -67,18 +67,18 @@ resource "aws_api_gateway_rest_api" "clue" {
 # API Gateway Resources
 resource "aws_api_gateway_resource" "api_resource_games" {
   path_part   = "games"
-  parent_id   = aws_api_gateway_rest_api.clue-backend.root_resource_id
-  rest_api_id = aws_api_gateway_rest_api.clue-backend.id
+  parent_id   = aws_api_gateway_rest_api.clue.root_resource_id
+  rest_api_id = aws_api_gateway_rest_api.clue.id
 }
 resource "aws_api_gateway_resource" "api_resource_game_id" {
   path_part   = "{game_id}"
   parent_id   = aws_api_gateway_resource.api_resource_games.id
-  rest_api_id = aws_api_gateway_rest_api.clue-backend.id
+  rest_api_id = aws_api_gateway_rest_api.clue.id
 }
 
 # API Gateway Methods
 resource "aws_api_gateway_method" "api_method_games" {
-  rest_api_id   = aws_api_gateway_rest_api.clue-backend.id
+  rest_api_id   = aws_api_gateway_rest_api.clue.id
   resource_id   = aws_api_gateway_resource.api_resource_games.id
   http_method   = "ANY"
   authorization = "NONE"
@@ -86,7 +86,7 @@ resource "aws_api_gateway_method" "api_method_games" {
 
 # API Gateway - Lambda Integrations and Permission
 resource "aws_api_gateway_integration" "lambda_api_integration_games" {
-  rest_api_id             = aws_api_gateway_rest_api.clue-backend.id
+  rest_api_id             = aws_api_gateway_rest_api.clue.id
   resource_id             = aws_api_gateway_resource.api_resource_games.id
   http_method             = aws_api_gateway_method.api_method_games.http_method
   integration_http_method = "POST"
@@ -98,5 +98,5 @@ resource "aws_lambda_permission" "lamda_api_permissions_games" {
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.game-controller.function_name
   principal     = "apigateway.amazonaws.com"
-  source_arn = "arn:aws:execute-api:us-east-1:228573559958:${aws_api_gateway_rest_api.clue-backend.id}/*/${aws_api_gateway_method.api_method_games.http_method}${aws_api_gateway_resource.api_resource_games.path}"
+  source_arn = "arn:aws:execute-api:us-east-1:228573559958:${aws_api_gateway_rest_api.clue.id}/*/${aws_api_gateway_method.api_method_games.http_method}${aws_api_gateway_resource.api_resource_games.path}"
 }
